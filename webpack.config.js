161a2da2a5b636415ab0c993359ab2b10a97b102
renjie_accenture <renjie_zhou@cathaypacific.com>
2020-04-30 18:14:05 +0800

    babel change

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    // 开发服务的配置
+    devServer: {
+        // 端口，默认8080
+        port: '8099',
+        // 进度条
+        progress: true,
+        // 启动压缩
+        //compress: true
+    },
+    // 监听
+    watch: true,
+    watchOptions: {
+        poll: 1000, // 每秒询问多少次
+        aggregateTimeout: 500,  //防抖 多少毫秒后再次触发
+        ignored: /node_modules/ //忽略时时监听
+    },
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -18,8 +34,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['react', '@babel/preset-env'],
-                        plugins: ['@babel/transform-runtime']
+                        presets: ['@babel/preset-react', '@babel/preset-env']
                     }
                 }
             },
